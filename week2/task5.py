b1a5cc4b54c4ac9bcc3fef302d5cba8f5a083a49
xxent41 <itsnurxan@gmail.com>
2025-12-24 15:18:03 +0500

    week2

--- week2/task5.py
+++ week2/task5.py
@@ -1,16 +1,18 @@
 valid_letters = set("ABCEHKMOPTXY")
 
 n = int(input())
+
 for _ in range(n):
     plate = input().strip()
 
-    if (
+    ok = (
         len(plate) == 6 and
         plate[0] in valid_letters and
-        plate[1:4].isdigit() and
+        plate[1].isdigit() and
+        plate[2].isdigit() and
+        plate[3].isdigit() and
         plate[4] in valid_letters and
         plate[5] in valid_letters
-    ):
-        print("Yes")
-    else:
-        print("No")
+    )
+
+    print("Yes" if ok else "No")
