af98f69e76c12c2643202c27025f467bfab7a859
xxent41 <itsnurxan@gmail.com>
2026-01-15 18:54:47 +0500

    fixed task2

--- week5/task2.py
+++ week5/task2.py
@@ -1,13 +1,16 @@
 import json
 
-with open("students.json", "r", encoding="utf-8") as f:
+input_file = "students.json"
+output_file = "students_with_averages.json"
+
+with open(input_file, "r", encoding="utf-8") as f:
     students = json.load(f)
 
 for student in students:
     grades = student["grades"]
     student["average_grade"] = round(sum(grades) / len(grades), 2)
 
-with open("students_with_averages.json", "w", encoding="utf-8") as f:
+with open(output_file, "w", encoding="utf-8") as f:
     json.dump(students, f, indent=2)
 
 print("Averages calculated.")
